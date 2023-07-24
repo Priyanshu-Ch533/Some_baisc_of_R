fad45a50269eeae6e530f5ea3124ca36a5bc6fd0
Priyanshu Chauhan <140386632+Priyanshu-Ch533@users.noreply.github.com>
2023-07-25 00:07:31 +0530

    Add files via upload

---
+++ Some Basic of R.R
@@ -0,0 +1,89 @@
+    R language
+    
+#Variable = Variable are Temperary storage spaces.
+  #like a container
+a = 'apple'
+b = 'boy'
+c = 'Cow'
+
+#and we can change a variable value also-
+a = 'Aam'
+b = 'baby' 
+c = 'cat'
+
+
+#Data Types - there are 4 types of data types Numerical, character,
+#             Logical, Complex
+num = 4
+class(num)
+
+cha =  'sam'
+class(cha)
+
+log = TRUE
+class(log)
+
+Complex = 7+4
+class(Complex)
+
+com = 7+4i
+class(com)
+
+
+#Operators - there are 4 types of Operators
+# Assignment Operators, Arithmentic Operators, Relational Operators,
+# Logical Operators
+
+#Assignment perators
+d = 1
+e <-1
+1 ->f
+
+#Arithmentic perators
+num1 = 10
+num2 = 20
+
+num1+num2
+
+num1-num2
+
+num1*num2
+
+num1/num2
+
+#Relational Operators
+
+num1 = 10
+num2 = 20
+
+num>num2
+
+num<num2
+
+num1==num2  # single (=) is like assign a value, 
+            # double (==) is check the value, using the value.
+num1!=num2  # != is equal for value.
+
+num2!=num1
+
+
+#logical Operators
+log1 = TRUE
+log2 = FALSE
+
+
+log1&log2
+
+log1&log1
+
+log2&log1
+
+log2&log2
+
+log1|log2
+
+log1|log1
+
+log2|log2
+
+log2|log1
